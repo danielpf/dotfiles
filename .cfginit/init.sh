b4921f35f9a2dd83d20241d270347b79c787e589
Daniel Fazeres <daniel.fazeres@ukg.com>
2022-12-23 23:52:55 +0000

    nvim

--- .cfginit/init.sh
+++ .cfginit/init.sh
@@ -1,4 +1,6 @@
 git clone --bare https://github.com/danielpf/dotfiles.git .cfg
 git --git-dir=.cfg/ --work-tree=$HOME checkout
 
+#wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+
 sudo apt-get install ripgrep
